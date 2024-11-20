097039c1fbe71646981cac61abc82d4f1dd70d12
irem <iremasutkalioglu@gmail.com>
2024-11-20 16:38:06 +0300

    done

--- app/actions/DeleteRoom.js
+++ app/actions/DeleteRoom.js
@@ -17,27 +17,29 @@ async function deleteRoom(roomId) {
       sessionCookie.value
     );
 
-    //Get User's ID
+    // Get user's ID
     const user = await account.get();
     const userId = user.$id;
 
-    // Fetch users Rooms
+    // Fetch users rooms
     const { documents: rooms } = await databases.listDocuments(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
       process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
       [Query.equal("user_id", userId)]
     );
-    //Find toom to delete
-    const roomToDelete = (room) => room.$id === roomId;
 
-    //Delete the room
+    // Find room to delete
+    const roomToDelete = rooms.find((room) => room.$id === roomId);
+
+    // Delete the room
     if (roomToDelete) {
       await databases.deleteDocument(
         process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
         process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
         roomToDelete.$id
       );
-      //Revalidate my rooms and all rooms
+
+      // Revalidate my rooms and all rooms
       revalidatePath("/rooms/my", "layout");
       revalidatePath("/", "layout");
 
@@ -50,7 +52,7 @@ async function deleteRoom(roomId) {
       };
     }
   } catch (error) {
-    console.log("Failed to delete room ", error);
+    console.log("Failed to delete room", error);
     return {
       error: "Failed to delete room",
     };
